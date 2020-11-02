07bfbffadf6b0028abc36c1eade51cf94fc50ee1
Tomas Cejka <tomas.cejka@styrax.cz>
2020-11-02 22:22:29 +0100

    * simple text change

--- openjfx-app-simple/src/main/java/cz/toce/openjfx/app/simple/MainApp.java
+++ openjfx-app-simple/src/main/java/cz/toce/openjfx/app/simple/MainApp.java
@@ -11,17 +11,15 @@ import org.slf4j.LoggerFactory;
 
 public class MainApp extends Application {
 
-    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MainApp.class);
 
     public static void main(String[] args) throws Exception {
         launch(args);
     }
 
     public void start(Stage stage) throws Exception {
-
-        log.info("Starting Hello JavaFX and Maven demonstration application");
-
-        log.debug("Showing JFX scene");
+        LOG.info("Starting Hello JavaFX and Maven demonstration application");
+        LOG.debug("Showing JFX scene");
         Label rootNode = new Label("Hello world!");
         rootNode.setAlignment(Pos.CENTER);
 		Scene scene = new Scene(rootNode , 400, 200);
