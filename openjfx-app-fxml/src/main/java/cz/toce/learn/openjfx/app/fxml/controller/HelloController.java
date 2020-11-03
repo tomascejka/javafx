c05ea1a782c4a9d86e0de7b763ec8740b94402bd
Tomas Cejka <tomas.cejka@styrax.cz>
2020-11-03 14:57:35 +0100

    * odebrani MigLayout z jednoducheho openjfx-app-fxml projektu, kt. predstavuje FXML jako standalone bez jakehokoli dalsiho pridavku, napr. MigLayoutu.

--- openjfx-app-fxml/src/main/java/cz/toce/learn/openjfx/app/fxml/controller/HelloController.java
+++ openjfx-app-fxml/src/main/java/cz/toce/learn/openjfx/app/fxml/controller/HelloController.java
@@ -1,46 +1,20 @@
 package cz.toce.learn.openjfx.app.fxml.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class HelloController
-{
-    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+public class HelloController {
+	private static final Logger log = LoggerFactory.getLogger(HelloController.class);
 
-    @FXML private TextField firstNameField;
-    @FXML private TextField lastNameField;
-    @FXML private Label messageLabel;
+	@FXML
+	private Text actiontarget;
 
-    public void sayHello() {
-
-        String firstName = firstNameField.getText();
-        String lastName = lastNameField.getText();
-
-        StringBuilder builder = new StringBuilder();
-
-        boolean isFirstNameNullOrBlank = (firstNameField == null || firstName.isBlank());
-        if (!isFirstNameNullOrBlank) {
-            builder.append(firstName);
-        }
-
-        if (!isFirstNameNullOrBlank) {
-            if (builder.length() > 0) {
-                builder.append(" ");
-            }
-            builder.append(lastName);
-        }
-
-        if (builder.length() > 0) {
-            String name = builder.toString();
-            log.debug("Saying hello to " + name);
-            messageLabel.setText("Hello " + name);
-        } else {
-            log.debug("Neither first name nor last name was set, saying hello to anonymous person");
-            messageLabel.setText("Hello mysterious person");
-        }
-    }
-
+	@FXML
+	protected void handleSubmitButtonAction(ActionEvent event) {
+		actiontarget.setText("Sign in button pressed");
+	}
 }
