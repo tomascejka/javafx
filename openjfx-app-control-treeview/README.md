477186af2fdceeab8e523824ce8519109dda1f72
Tomas Cejka <tomas.cejka@styrax.cz>
2020-11-05 01:29:04 +0100

    + novy projekt, kde zkusim TreeView komponentu

---
+++ openjfx-app-control-treeview/README.md
@@ -0,0 +1,21 @@
+# openjfx-app-control-treeview
+Velmi jednoduchá aplikace - "helloworld", presentující použití [openjfx](https://github.com/openjdk/jfx/) (implementace Java FX) a komponentu TreeView.
+
+## Předpoklady
+V případě, že si budete chtít spustit, anebo si hrát se zdrojovými kódy, tak jsem to ladil s těmito nástroji:
+* [oracle jdk v11+](https://www.oracle.com/java/technologies/javase-downloads.html)
+* [apache maven v 3.6.3](https://archive.apache.org/dist/maven/maven-3/3.6.3/binaries/)
+* [openjxf](https://openjfx.io/index.html), [další(https://github.com/openjdk/jfx/)
+* [openjxf maven plugin](https://github.com/openjfx/javafx-maven-plugin)
+
+## Použití
+Stačí spustit v root adresáři v kterém je ```pom.xml``` soubor příkaz ```javafx:run```:
+```
+mvn clean javafx:run
+```
+
+## Zdroje
+* https://docs.oracle.com/javafx/2/ui_controls/tree-view.htm#
+* https://docs.oracle.com/javase/8/javafx/api/javafx/scene/control/TreeView.html
+* https://examples.javacodegeeks.com/core-java/javafx-treeview-example/
+* http://www.kware.net/?p=204 << Filtering a JavaFX TreeView
---
+++ openjfx-app-control-treeview/src/main/java/cz/toce/openjfx/app/simple/MainApp.java
@@ -0,0 +1,33 @@
+package cz.toce.openjfx.app.simple;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainApp extends Application {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MainApp.class);
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        LOG.info("Starting Hello JavaFX and Maven demonstration application");
+        LOG.debug("Showing JFX scene");
+        Label rootNode = new Label("Hello world!");
+        rootNode.setAlignment(Pos.CENTER);
+		Scene scene = new Scene(rootNode , 400, 200);
+        scene.getStylesheets().add("/styles/styles.css");
+
+        stage.setTitle("Hello JavaFX and Maven");
+        stage.getIcons().add(new Image("https://cdn3.iconfinder.com/data/icons/inficons/512/stackoverflow.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+}
